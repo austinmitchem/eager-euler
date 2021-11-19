b7e0712feeaa1b1d4afece4159fd3828fef1a8a9
Austin Duke Mitchem <42751791+austinmitchem@users.noreply.github.com>
2021-11-18 21:24:34 -0600

    feat: new super duper fancy line (#27)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is a new feature!
+          And this is a new line.
         </p>
         <p>
           Something else!!!
