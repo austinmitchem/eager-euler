2a27c5333926014aaa3f517929e393b1b892acfc
Austin Duke Mitchem <42751791+austinmitchem@users.noreply.github.com>
2021-11-21 15:40:37 -0600

    feat: new feature 2.0 (#78)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is a new feature!
+          New feature 2.0
         </p>
         <a className="App-link"
           href="https://reactjs.org"
